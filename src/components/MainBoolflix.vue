b1b0b09b900ec8df7eb3817f3046d27eb5a8fde5
camillapalombi <camilla.palombi14@gmail.com>
2022-03-29 12:44:02 +0200

    Collegata API alla stringa di ricerca

--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -6,14 +6,28 @@
 
 <script>
 import BoxFilm from './BoxFilm.vue';
+import axios from 'axios';
+
 export default {
 name: 'MainBoolflix',
+data () {
+    return {
+        caractherFilms: null
+    }
+},
 components: {
     BoxFilm
 },
 props: {
     stringSearchFilm: String
   },
+  created () {
+      setTimeout(() => {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=7f0f1cb86088f95987911722d21959f7&language=en-US&query=${this.stringSearchFilm}&page=1&include_adult=false`).then((response) => {
+          this.caractherFilms = response.data.results;
+          console.log(this.caractherFilms);
+      });
+  }, 6000)}
 }
 </script>
 
