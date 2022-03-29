809731d71dc84388568075acebd1f2896dd99131
camillapalombi <camilla.palombi14@gmail.com>
2022-03-29 12:59:55 +0200

    corretto errore lingua

--- src/components/BoxFilm.vue
+++ src/components/BoxFilm.vue
@@ -3,7 +3,7 @@
 
       <h2 class="title">Titolo: {{ dataFilms.title}}</h2>
       <h3 class="original-title">Titolo originale: {{ dataFilms.original_title}}</h3>
-      <div class="lenguage">Lingua: {{ dataFilms.original_lenguage}} </div>
+      <div class="lenguage">Lingua: {{ dataFilms.original_language}} </div>
       <div class="rating">Voto: {{ dataFilms.vote_average}}</div>
 
   </div>
